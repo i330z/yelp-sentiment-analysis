a191c48a3dddc6bd3fcc9bd93e61aad1de31fd1b
i330z <izzazahmed134@gmail.com>
2024-06-30 21:05:01 +0530

    code fixes

--- main.py
+++ main.py
@@ -8,14 +8,18 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 
 def extract_data(url):
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        regex = re.compile('.*comment.*')
-        results = soup.find_all('p',{'class':regex})
-        reviews = [result.text for result in results]
-        df =  pd.DataFrame(np.array(reviews), columns=['reviews'])
-        return df
-
+        try:
+                print('extracting')
+                r = requests.get(url)
+                soup = BeautifulSoup(r.text, 'html.parser')
+                regex = re.compile('.*comment.*')
+                results = soup.find_all('p',{'class':regex})
+                reviews = [result.text for result in results]
+                df =  pd.DataFrame(np.array(reviews), columns=['reviews'])
+                return df
+        except Exception as e:
+               st.error(f"Error extracting data: {e}")
+               return None
 
 tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
@@ -25,38 +29,48 @@ model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-mu
 # print(result.logits)
 
 def sentiment_analysis(review):
-        token = tokenizer.encode(review, return_tensors='pt')
-        result = model(token)
-        return int(torch.argmax(result.logits))+1
-
+        try:
+                token = tokenizer.encode(review, return_tensors='pt')
+                result = model(token)
+                return int(torch.argmax(result.logits))+1
+        except Exception as e:
+                st.error(f"Error performing sentiment analysis: {e}")
+                return None
 
 
 #Streamlit Code
 st.title("Yelp Comment Sentiment Analysis")
-url = st.text_input("Enter the URL:")
 
+def clear_input():
+        st.session_state["input_text"]
+
+if "input_text" not in st.session_state:
+        st.session_state["input_text"] = ""
+
+
+url = st.text_input("Enter the URL:", key="input_text")
 
 if url:
     st.write(f"The entered URL is: {url}")
     # data = extract_data('https://www.yelp.com/biz/social-brew-cafe-pyrmont')
     st.write("Generating Sentiments...")
 
-
     data =  extract_data(url)
-    # print(data['reviews'])
-    review1 = data['reviews']
-    emoji = ['🤢','🥴','🙃','🙂','😍']
-    list_data= []
-    for review in review1:
-            rev_senti = sentiment_analysis(review)
-            print(type(rev_senti))
-            emoji_senti = emoji[rev_senti - 1]
-            list_data.append({ "review": review, "sentiment": emoji_senti })    
-            # print(list_data)
-            
-    df = pd.DataFrame(list_data)
+        # print(data['reviews'])
+    if not data.empty:
+        print(data)
+        review1 = data['reviews']
+        emoji = ['🤢','🥴','🙃','🙂','😍']
+        list_data= []
+        for review in review1:
+                rev_senti = sentiment_analysis(review)
+                emoji_senti = emoji[rev_senti - 1]
+                list_data.append({ "review": review, "sentiment": emoji_senti })    
+                # print(list_data)
+                
+        df = pd.DataFrame(list_data)
 
-    print(df)
-    
-    st.write("User Sentiment Rating:", df )
-   
+        # print(df)
+        
+        st.write("User Sentiment Rating:", df )
+        clear_input()
